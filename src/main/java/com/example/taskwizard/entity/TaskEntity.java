e32211d455b3e15effd58c9c2d6f2460d80a5aff
mustgamedev <mustafashahdzahan@gmail.com>
2023-11-30 11:20:27 +0300

    - Unfolded jakarta imports for better readability - Replaced @Data annotation with @Getter, @Setter due lombok issues - Changed @GeneratedType strategy from Auto to Identity - Added toString equals and hashcode methods manually

--- src/main/java/com/example/taskwizard/entity/TaskEntity.java
+++ src/main/java/com/example/taskwizard/entity/TaskEntity.java
@@ -6,26 +6,57 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+import java.util.Objects;
+
+
 @Entity
+@Getter
+@Setter
 @NoArgsConstructor
 @Table(name = "tasks")
-public class TaskEntity
-{
+public class TaskEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
     @Column(name = "description")
     private String description;
+
     @Column(name = "status")
     private boolean isCompleted;
 
-    public TaskEntity(String description)
-    {
+    public TaskEntity(String description) {
         this.description = description;
         this.isCompleted = false;
     }
+
+
+    //region toString, equals, hashCode
+    @Override
+    public String toString() {
+        return "TaskEntity{" + "id=" + id + ", description='" + description + '\'' + ", isCompleted=" + isCompleted + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        TaskEntity that = (TaskEntity) o;
+        return isCompleted == that.isCompleted && Objects.equals(id, that.id) && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, description, isCompleted);
+    }
+    //endregion
 }
