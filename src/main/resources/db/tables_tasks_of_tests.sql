50ea7192193ee7baf7b5494fd5905ff5e2f4b278
mustgamedev <mustafashahdzahan@gmail.com>
2023-12-28 02:54:21 +0300

    Created mysql script for adding test data to DB tables.

---
+++ src/main/resources/db/tables_tasks_of_tests.sql
@@ -0,0 +1,7 @@
+-- Adding testing data to db table.
+INSERT INTO mst_db.tasks (id, description, status) VALUES (1, 'Read a book', 0);
+INSERT INTO mst_db.tasks (id, description, status) VALUES (2, 'Study java', 0);
+INSERT INTO mst_db.tasks (id, description, status) VALUES (3, 'Go to bed early today', 0);
+INSERT INTO mst_db.tasks (id, description, status) VALUES (4, 'Learn english', 0);
+INSERT INTO mst_db.tasks (id, description, status) VALUES (5, 'Go to school', 0);
+INSERT INTO mst_db.tasks (id, description, status) VALUES (6, 'Eat healthy food', 0);
