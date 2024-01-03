90c1bd4a4f0f26797e6c9cff75c88c7dc70a11ab
mustgamedev <mustafashahdzahan@gmail.com>
2024-01-03 21:01:40 +0300

    Added jsDoc comment on method.

--- src/main/resources/static/scripts/string-validator.js
+++ src/main/resources/static/scripts/string-validator.js
@@ -1,7 +1,11 @@
+/**
+ * Checks the form for an empty value in the task_description field.
+ * If the field is empty, displays an alert and clears the field.
+ */
 function validateForm() {
     const x = document.forms["myForm"]["task_description"].value;
     if (x == null || x.trim().length === 0) {
-        alert("Empty string");
+        window.alert("Empty string");
         x.clear();
     }
 }
