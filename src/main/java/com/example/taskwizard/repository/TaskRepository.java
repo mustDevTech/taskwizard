705836a6fad35d313ec2ec8d06d61d1bb7824efe
mustgamedev <mustafashahdzahan@gmail.com>
2024-03-08 06:37:28 +0300

    Added custom method updateTaskStatus to TaskRepository for updating task status in the database.

--- src/main/java/com/example/taskwizard/repository/TaskRepository.java
+++ src/main/java/com/example/taskwizard/repository/TaskRepository.java
@@ -1,10 +1,17 @@
 package com.example.taskwizard.repository;
 
 import com.example.taskwizard.entity.TaskEntity;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository for {@link TaskEntity}
+ */
 @Repository
-public interface TaskRepository extends CrudRepository<TaskEntity, Long>
-{
+public interface TaskRepository extends CrudRepository<TaskEntity, Long> {
+    @Modifying
+    @Query(nativeQuery = true, value = "UPDATE tasks SET status = ?2 WHERE id = ?1")
+    void updateTaskStatus(Long id, Boolean completed);
 }
