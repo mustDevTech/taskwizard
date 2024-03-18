b21ed9187cf3c06d36a4a0f012469a7718cbf390
mustgamedev <mustafashahdzahan@gmail.com>
2024-03-18 03:37:16 +0300

    Converted class into Record for better readability, stability.

--- src/main/java/com/example/taskwizard/dto/TaskDto.java
+++ src/main/java/com/example/taskwizard/dto/TaskDto.java
@@ -1,58 +1,13 @@
 package com.example.taskwizard.dto;
 
 import com.example.taskwizard.entity.TaskEntity;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.experimental.FieldDefaults;
-
 import java.io.Serializable;
-import java.util.Objects;
 
 /**
- * Dto of entity {@link TaskEntity}
+ * Dto record of entity {@link TaskEntity}
  */
-@Getter
-@Setter
-@NoArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE)
-public class TaskDto implements Serializable {
-    Long id;
-    String description;
-    Boolean isCompleted;
-
+public record TaskDto(Long id, String description, Boolean isCompleted) implements Serializable {
     public TaskDto(String description) {
-        this.id = null;
-        this.description = description;
-        this.isCompleted = false;
+        this(null, description, false);
     }
-
-    //region toString, equals, hashCode
-    @Override
-    public String toString() {
-        return "TaskDto{" +
-                "id=" + id +
-                ", description='" + description + '\'' +
-                ", isCompleted=" + isCompleted +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        TaskDto taskDto = (TaskDto) o;
-        return Objects.equals(id, taskDto.id) && Objects.equals(description, taskDto.description) && Objects.equals(isCompleted, taskDto.isCompleted);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, description, isCompleted);
-    }
-    //endregion
 }
